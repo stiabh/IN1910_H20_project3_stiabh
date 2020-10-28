b2fe31c3637f1e44c03463b2c80938378524c96b
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-10-28 13:03:26 +0100

    Completed 2b). Added methods to generate and plot n-gon corners.

--- chaos_game.py
+++ chaos_game.py
@@ -13,4 +13,26 @@ class ChaosGame():
             raise ValueError("n must be greater than or equal to 3")
         if r <= 0 or r >= 1:
             raise ValueError("r must be in range (0, 1)")
+
         self.n, self.r = n, r
+        self._generate_ngon()
+
+    def _generate_ngon(self):
+        """Generate corners of n-gon."""
+        theta = np.linspace(0, 2*np.pi, self.n, endpoint=False)
+        corners = np.empty(shape=(2, self.n))
+        corners[0], corners[1] = np.sin(theta), np.cos(theta)
+        self.corners = np.transpose(corners)
+
+    def plot_ngon(self):
+        """Plot n-gon corners."""
+        plt.figure()
+        plt.scatter(*zip(*self.corners))
+        plt.axis("equal")
+
+
+if __name__ == "__main__":
+    for n in range(3, 9):
+        cg = ChaosGame(n)
+        cg.plot_ngon()
+    plt.show()
