39ab6886572be87f1abd62198e2209030cb3b879
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-10-29 11:15:18 +0100

    Completed 3b). Defined 4 affine transformations

--- fern.py
+++ fern.py
@@ -13,3 +13,14 @@ class AffineTransform():
 
         a, b, c, d, e, f = self.a, self.b, self.c, self.d, self.e, self.f
         return np.dot(np.array([[a, b], [c, d]]), point) + np.array([e, f])
+
+
+if __name__ == "__main__":
+    barnsley = [(0, 0, 0, 0.16, 0, 0),
+                (0.85, 0.04, -0.04, 0.85, 0, 1.60),
+                (0.20, -0.26, 0.23, 0.22, 0, 1.60),
+                (-0.15, 0.28, 0.26, 0.24, 0, 0.44)]
+    f1 = AffineTransform(*barnsley[0])
+    f2 = AffineTransform(*barnsley[1])
+    f3 = AffineTransform(*barnsley[2])
+    f4 = AffineTransform(*barnsley[3])
