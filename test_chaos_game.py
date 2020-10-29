9a84ed6331b31be13d0c529de0530f9e255f5687
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-10-29 09:58:26 +0100

    Better function names

--- test_chaos_game.py
+++ test_chaos_game.py
@@ -17,16 +17,16 @@ def test_ChaosGame_params(n, r):
         cg = ChaosGame(n, r)
 
 
-def test__generate_ngon():
+def test_ChaosGame_generate_ngon():
     tol = 1e-10
-    corners = np.array([[0.,  1.],
-                        [0.8660254037844387, -0.4999999999999998],
-                        [-0.8660254037844385, -0.5000000000000004]])
+    corners = np.array([(0.,  1.),
+                        (0.8660254037844387, -0.4999999999999998),
+                        (-0.8660254037844385, -0.5000000000000004)])
     cg = ChaosGame(3, 0.5)
     assert np.all(abs(cg.corners - corners) < 1e-10)
 
 
-def test_iterate():
+def test_ChaosGame_iterate():
     n = 1000
     cg = ChaosGame(3, 0.5)
     cg.iterate(n)
@@ -34,7 +34,7 @@ def test_iterate():
     assert len(cg.J == n)
 
 
-def test_plot():
+def test_ChaosGame_plot():
     cg = ChaosGame(3, 0.5)
     with pytest.raises(AttributeError):
         cg.plot()
